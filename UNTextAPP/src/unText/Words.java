9902daeccf5e13a3662bb6d926044cad372be05f
Sydney Crumley <srsimps19@gmail.com>
2019-06-04 14:45:43 -0500

    built out words class

---
+++ UNTextAPP/src/unText/Words.java
@@ -0,0 +1,21 @@
+package unText;
+
+public class Words
+{
+    private String word;
+
+    public Words(String word)
+    {
+        this.word = word;
+    }
+
+    public String getWord() 
+    {
+        return word;
+    }
+
+    public void setWord(String word) 
+    {
+        this.word = word;
+    }
+}
