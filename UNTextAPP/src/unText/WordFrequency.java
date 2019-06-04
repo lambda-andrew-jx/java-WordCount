3a5d8a45e8afe28fea9a56112f81669d61a6d27c
Sydney Crumley <srsimps19@gmail.com>
2019-06-04 18:16:26 -0500

    fixed issues lots of issues

--- UNTextAPP/src/unText/WordFrequency.java
+++ UNTextAPP/src/unText/WordFrequency.java
@@ -1,12 +1,12 @@
 package unText;
 
+import java.util.*;
+
 public class WordFrequency {
     private String text;
-    private HashMap<String, Integer> wordCount = new HashMap<>();
+    public HashMap<String, Integer> wordcount = new HashMap<>();
 
     public WordFrequency(String text) {
         this.text = text;
-        cleanText();
-        addWordsToMap();
     }
 }
